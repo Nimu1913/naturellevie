f191a83d5e57a2bde4118a7393b1c7d897f8584e
nicholas <nicholas.muradov@live.se>
2026-01-02 15:46:56 +0100

    Add Pages Function to set correct MIME types for JS modules

---
+++ functions/_headers.ts
@@ -0,0 +1,16 @@
+export async function onRequest({ request, next }: { request: Request; next: () => Promise<Response> }) {
+  const response = await next();
+  
+  // Set correct MIME types for JavaScript modules
+  const url = new URL(request.url);
+  if (url.pathname.endsWith('.js') || url.pathname.endsWith('.mjs')) {
+    response.headers.set('Content-Type', 'application/javascript; charset=utf-8');
+  } else if (url.pathname.endsWith('.wasm')) {
+    response.headers.set('Content-Type', 'application/wasm');
+  } else if (url.pathname.endsWith('.json')) {
+    response.headers.set('Content-Type', 'application/json; charset=utf-8');
+  }
+  
+  return response;
+}
+
